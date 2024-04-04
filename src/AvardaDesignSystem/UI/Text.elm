b50cdfd8ccdbf8218d24f013fdd4eb8be744f012
Martin Bures <mbur@ciklum.com>
2024-04-04 13:58:31 +0200

    added paragraph as POC

---
+++ src/AvardaDesignSystem/UI/Text.elm
@@ -0,0 +1,10 @@
+module AvardaDesignSystem.UI.Text exposing (..)
+
+import Html exposing (Attribute, Html)
+
+paragraph : List (Attribute msg) -> List (Html msg) -> Html msg
+paragraph =
+  Html.p 
+
+
+
